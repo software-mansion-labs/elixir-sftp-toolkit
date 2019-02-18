542f9f4e6830e0755fcd0d79d9df271225e421a1
Marcin Lewandowski <marcin.lewandowski@swmansion.com>
2019-02-18 16:41:09 +0100

    Updated docs

--- lib/sftp_toolkit/recursive.ex
+++ lib/sftp_toolkit/recursive.ex
@@ -1,7 +1,7 @@
 defmodule SFTPToolkit.Recursive do
   @moduledoc """
-  Module containing functions that allow to do recursive 
-  operations on the directories.  
+  Module containing functions that allow to do recursive
+  operations on the directories.
   """
 
   @default_operation_timeout 5000
@@ -19,7 +19,7 @@ defmodule SFTPToolkit.Recursive do
 
   ## Options
 
-  * `operation_timeout` - SFTP operation timeout (it is a timeout 
+  * `operation_timeout` - SFTP operation timeout (it is a timeout
      per each SFTP operation, not total timeout), defaults to 5000 ms.
 
   ## Limitations
@@ -38,17 +38,17 @@ defmodule SFTPToolkit.Recursive do
 
   * `{:make_dir, info}` - `:ssh_sftp.make_dir/3` failed and `info`
     contains the underlying error returned from it,
-  * `{:file_info, info}` - `:ssh_sftp.read_file_info/3` failed and 
+  * `{:file_info, info}` - `:ssh_sftp.read_file_info/3` failed and
     `info` contains the underlying error returned from it,
   * `{:invalid_type, path, type} - one of the components of the
     path to create, specified as `path` is not a directory, and
     it's actual type is specified as `type`,
-  * `{:invalid_access, path, access}` - one of the components of 
+  * `{:invalid_access, path, access}` - one of the components of
     the path to create, specified as `path` is a directory, but
-    it's access is is invalid and it's actual access mode is 
+    it's access is is invalid and it's actual access mode is
     specified as `access`.
 
-  ## Notes 
+  ## Notes
 
   ### Implementation details
 
@@ -57,21 +57,21 @@ defmodule SFTPToolkit.Recursive do
   error code to distinguish a real error from a case where directory
   just exists and we can proceed.
 
-  Moreover, the path component may exist but it can be a regular file 
-  which will prevent us from creating a subdirectory. 
+  Moreover, the path component may exist but it can be a regular file
+  which will prevent us from creating a subdirectory.
 
   Due to these limitations we're checking if directory exists
   prior to each creation of a directory as we can't rely on the
-  returned error reasons, even if we use newer versions of SFTP they 
+  returned error reasons, even if we use newer versions of SFTP they
   tend to return more fine-grained information.
 
   ### Timeouts
 
   It was observed in the wild that underlying `:ssh_sftp.list_dir/3`
-  and `:ssh_sftp.read_file_info/3` always returned `{:error, :timeout}` 
+  and `:ssh_sftp.read_file_info/3` always returned `{:error, :timeout}`
   with some servers when SFTP version being used was greater than 3,
-  at least with Elixir 1.7.4 and Erlang 21.0. If you encounter such 
-  issues try passing `{:sftp_vsn, 3}` option while creating a SFTP 
+  at least with Elixir 1.7.4 and Erlang 21.0. If you encounter such
+  issues try passing `{:sftp_vsn, 3}` option while creating a SFTP
   channel.
   """
   @spec make_dir_recursive(pid, Path.t, [operation_timeout: timeout]) :: :ok | {:error, any}
@@ -113,9 +113,9 @@ defmodule SFTPToolkit.Recursive do
     end
   end
 
-  
+
   @doc """
-  Recursively lists files in a given directory over existing SFTP 
+  Recursively lists files in a given directory over existing SFTP
   channel.
 
   ## Arguments
@@ -128,60 +128,61 @@ defmodule SFTPToolkit.Recursive do
 
   ## Options
 
-  * `operation_timeout` - SFTP operation timeout (it is a timeout 
+  * `operation_timeout` - SFTP operation timeout (it is a timeout
      per each SFTP operation, not total timeout), defaults to 5000 ms,
   * `included_types` - which file types should be included in the
-     result, defaults to `[:regular]`. See the `:file.file_info` 
+     result, defaults to `[:regular]`. See the `:file.file_info`
      typespec for list of all valid values,
-  * `result_format` - can be one of `:path` or `:file_info`. 
-  
+  * `result_format` - can be one of `:path` or `:file_info`.
+
      If you pass `:path`, the result will be a list of strings containing
      file names.
-     
+
      If you pass `:file_info`, the result will be a list of `{path, file_info}`
-     tuples, where `file_info` will have have the same format as 
-     `:file.file_info`. 
+     tuples, where `file_info` will have have the same format as
+     `:file.file_info`. Please note that if you return `:skip_but_include`
+     from the `iterate_callback` the `file_info` will be `:undefined`.
 
      Defaults to `:path`.
-  * `recurse_callback` - optional function that will be called 
-     before recursing to the each subdirectory that is found. It will 
-     get one argument that is a path currently being evaluated and should 
-     return one of `:skip`, `:skip_but_include` or `:ok`. 
-     
-     If it will return `:skip`, the whole tree, including the path passed 
-     as an argument to the function will be skipped and they won't be 
+  * `recurse_callback` - optional function that will be called
+     before recursing to the each subdirectory that is found. It will
+     get one argument that is a path currently being evaluated and should
+     return one of `:skip`, `:skip_but_include` or `:ok`.
+
+     If it will return `:skip`, the whole tree, including the path passed
+     as an argument to the function will be skipped and they won't be
      included in the final result.
-     
-     If it will return `:skip_but_include`, the underlying tree, except 
-     the path passed as an argument to the function will be skipped 
+
+     If it will return `:skip_but_include`, the underlying tree, except
+     the path passed as an argument to the function will be skipped
      won't be included in the final result but the path itself will,
      as long as it's type is within included_types.
 
      If it will return `:ok`, it will recurse, and this is also
      the default behaviour if function is not passed.
-  * `iterate_callback` - optional function that will be called 
-     before evaluating each file that is found whether it is a directory. 
-     It will get one argument that is a path currently being evaluated 
-     and should return one of `:skip`, `:skip_but_include` or `:ok`. 
-     
+  * `iterate_callback` - optional function that will be called
+     before evaluating each file that is found whether it is a directory.
+     It will get one argument that is a path currently being evaluated
+     and should return one of `:skip`, `:skip_but_include` or `:ok`.
+
      If it will return `:skip`, the file will not be evaluated for its
      type and it will not be included in the final result.
-     
+
      If it will return `:skip_but_include`, the file will not be evaluated
      for its type but it will be always included in the final result.
 
      If it will return `:ok`, it will evaluate file's type and try recurse
-     if it's directory, and this is also the default behaviour if function 
+     if it's directory, and this is also the default behaviour if function
      is not passed.
 
   The `recurse_callback` and `iterate_callback` options are useful if you
-  traverse a large tree and you can determine that only certain parts of it 
+  traverse a large tree and you can determine that only certain parts of it
   are meaningful solely from the paths or file names. For example if your
-  directories are created programatically, and you know that files with 
-  the `.pdf` extension are always regular files and by no means they 
-  are directories you can instruct this function that it's pointless to 
-  read their file information. Thanks to this you can limit amount of 
-  calls to `:ssh_sftp.read_file_info/3` just by checking if given path 
+  directories are created programatically, and you know that files with
+  the `.pdf` extension are always regular files and by no means they
+  are directories you can instruct this function that it's pointless to
+  read their file information. Thanks to this you can limit amount of
+  calls to `:ssh_sftp.read_file_info/3` just by checking if given path
   has an appropriate suffix and returning the appropriate value.
 
   ## Limitations
@@ -197,32 +198,32 @@ defmodule SFTPToolkit.Recursive do
 
   On error returns `{:error, reason}`, where `reason` might be one
   of the following:
-  
+
   * `{:invalid_type, path, type} - given path is not a directory and
     it's actual type is specified as `type`,
   * `{:invalid_access, path, access}` - given path is a directory, but
-    it's access is is invalid and it's actual access mode is specified 
+    it's access is is invalid and it's actual access mode is specified
     as `access`.
   * `{:list_dir, info}` - `:ssh_sftp.list_dir/3` failed and `info`
     contains the underlying error returned from it,
-  * `{:file_info, info}` - `:ssh_sftp.read_file_info/3` failed and 
+  * `{:file_info, info}` - `:ssh_sftp.read_file_info/3` failed and
     `info` contains the underlying error returned from it.
 
-  ## Notes 
+  ## Notes
 
   ### Timeouts
 
   It was observed in the wild that underlying `:ssh_sftp.list_dir/3`
-  and `:ssh_sftp.read_file_info/3` always returned `{:error, :timeout}` 
+  and `:ssh_sftp.read_file_info/3` always returned `{:error, :timeout}`
   with some servers when SFTP version being used was greater than 3,
-  at least with Elixir 1.7.4 and Erlang 21.0. If you encounter such 
-  issues try passing `{:sftp_vsn, 3}` option while creating a SFTP 
+  at least with Elixir 1.7.4 and Erlang 21.0. If you encounter such
+  issues try passing `{:sftp_vsn, 3}` option while creating a SFTP
   channel.
   """
   @spec list_dir_recursive(pid, Path.t, [
     operation_timeout: timeout,
-    result_format: :path | :file_info, 
-    included_types: [:device | :directory | :other | :regular | :symlink | :undefined], 
+    result_format: :path | :file_info,
+    included_types: [:device | :directory | :other | :regular | :symlink | :undefined],
     recurse_callback: nil | (Path.t -> :skip | :skip_but_include | :ok),
     iterate_callback: nil | (Path.t -> :skip | :skip_but_include | :ok),
   ]) :: {:ok, [] | [Path.t | {Path.t, :file.file_info}]} | {:error, any}
@@ -270,7 +271,7 @@ defmodule SFTPToolkit.Recursive do
     included_types = Keyword.get(options, :included_types, [:regular])
     iterate_callback = Keyword.get(options, :iterate_callback, nil)
     recurse_callback = Keyword.get(options, :recurse_callback, nil)
-    
+
     # Call iterate_callback function only once, if it's present and store the return value
     iterate_callback_result = if !is_nil(iterate_callback) do
       iterate_callback.(path_full)
@@ -314,7 +315,7 @@ defmodule SFTPToolkit.Recursive do
             # If we're not allowed to recurse, honour instructions received from the recurse_callback function
             case recurse_callback_result do
               :skip_but_include ->
-                do_list_dir_iterate(sftp_channel_pid, path, tail, options, [result_item|acc])              
+                do_list_dir_iterate(sftp_channel_pid, path, tail, options, [result_item|acc])
               :skip ->
                 do_list_dir_iterate(sftp_channel_pid, path, tail, options, acc)
             end
@@ -330,7 +331,7 @@ defmodule SFTPToolkit.Recursive do
             :file_info ->
               {path_full, file_info}
           end
-          
+
           # Add it to the result it if its type was listed in the included_typesd
           acc = if type in included_types do
             [result_item|acc]
@@ -367,5 +368,5 @@ defmodule SFTPToolkit.Recursive do
           do_list_dir_iterate(sftp_channel_pid, path, tail, options, acc)
       end
     end
-  end  
+  end
 end
