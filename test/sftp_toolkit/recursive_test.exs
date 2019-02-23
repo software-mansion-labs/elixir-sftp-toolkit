d99b138f237be6b542af2269fd4d476065d21538
Marcin Lewandowski <marcin.lewandowski@swmansion.com>
2019-02-23 20:10:54 +0100

    Extracted common code related to the SSH deamon to the support module

--- test/sftp_toolkit/recursive_test.exs
+++ test/sftp_toolkit/recursive_test.exs
@@ -1,64 +1,7 @@
 defmodule SFTPToolkit.RecursiveTest do
   use ExUnit.Case, async: true
+  use SFTPToolkit.Support.SSHDaemon
   alias SFTPToolkit.Recursive
-
-  setup do
-    tempdir =
-      cond do
-        !is_nil(System.get_env("TMPDIR")) ->
-          System.get_env("TMPDIR")
-
-        !is_nil(System.get_env("TMP")) ->
-          System.get_env("TMP")
-
-        !is_nil(System.get_env("TEMP")) ->
-          System.get_env("TEMP")
-
-        true ->
-          "/tmp"
-      end
-      |> Path.join("sftp_toolkit_test")
-      |> Path.join(to_string(Enum.random(1_000_000..9_999_999)))
-
-    # TODO repeat instead of throw
-    if File.exists?(tempdir), do: throw(:tempdir_exist)
-
-    :ok = File.mkdir_p!(tempdir)
-
-    {:ok, daemon_ref} =
-      {:ok, ssh_daemon_ref} =
-      :ssh.daemon(:loopback, 0,
-        user_passwords: [{'someuser', 'somepassword'}],
-        system_dir: Path.join([System.cwd!(), "test", "extra", "ssh"]) |> to_charlist,
-        subsystems: [
-          :ssh_sftpd.subsystem_spec(cwd: tempdir |> to_charlist)
-        ]
-      )
-
-    {:ok, [port: port, ip: ip, profile: :default]} = :ssh.daemon_info(daemon_ref)
-
-    {:ok, sftp_channel_pid, ssh_connection_ref} =
-      :ssh_sftp.start_channel(ip, port,
-        user: 'someuser',
-        password: 'somepassword',
-        user_interaction: false,
-        silently_accept_hosts: true
-      )
-
-    on_exit(:ssh_client, fn ->
-      :ssh.close(ssh_connection_ref)
-    end)
-
-    on_exit(:ssh_daemon, fn ->
-      :ssh.stop_daemon(ssh_daemon_ref)
-    end)
-
-    on_exit(:del, fn ->
-      File.rm_rf!(tempdir)
-    end)
-
-    [sftp_channel_pid: sftp_channel_pid, tempdir: tempdir]
-  end
 
   describe "make_dir_recursive/2" do
     test "if given path is valid and contains just one level and there's no file or directory with such name, returns :ok",
@@ -73,6 +16,8 @@ defmodule SFTPToolkit.RecursiveTest do
       assert Path.join(tempdir, "**") |> Path.wildcard() == [
                Path.join(tempdir, "testdir")
              ]
+      {:ok, %File.Stat{type: type}} = File.stat(Path.join(tempdir, "testdir"))
+      assert type == :directory
     end
 
     test "if given path is valid and contains many levels and there's no file or directory with such name, returns :ok",
@@ -80,7 +25,7 @@ defmodule SFTPToolkit.RecursiveTest do
       assert Recursive.make_dir_recursive(sftp_channel_pid, "testdir1/testdir2") == :ok
     end
 
-    test "if given path is valid and contains many levels and there's no file or directory with such name, creates a single directory",
+    test "if given path is valid and contains many levels and there's no file or directory with such name, creates multiple directories",
          %{sftp_channel_pid: sftp_channel_pid, tempdir: tempdir} do
       Recursive.make_dir_recursive(sftp_channel_pid, "testdir1/testdir2")
 
@@ -88,6 +33,10 @@ defmodule SFTPToolkit.RecursiveTest do
                Path.join(tempdir, "testdir1"),
                Path.join([tempdir, "testdir1", "testdir2"])
              ]
+      {:ok, %File.Stat{type: type1}} = File.stat(Path.join(tempdir, "testdir1"))
+      assert type1 == :directory
+      {:ok, %File.Stat{type: type2}} = File.stat(Path.join([tempdir, "testdir1", "testdir2"]))
+      assert type2 == :directory
     end
 
     test "if given path is valid and contains many levels and some levels are already present and they are directories, returns :ok",
@@ -97,7 +46,7 @@ defmodule SFTPToolkit.RecursiveTest do
       assert Recursive.make_dir_recursive(sftp_channel_pid, "testdir1/testdir2") == :ok
     end
 
-    test "if given path is valid and contains many levels and some levels are already present and they are directories, creates a single directory",
+    test "if given path is valid and contains many levels and some levels are already present and they are directories, creates missing directories",
          %{sftp_channel_pid: sftp_channel_pid, tempdir: tempdir} do
       File.mkdir_p!(Path.join(tempdir, "testdir1"))
 
@@ -107,6 +56,8 @@ defmodule SFTPToolkit.RecursiveTest do
                Path.join(tempdir, "testdir1"),
                Path.join([tempdir, "testdir1", "testdir2"])
              ]
+      {:ok, %File.Stat{type: type2}} = File.stat(Path.join([tempdir, "testdir1", "testdir2"]))
+      assert type2 == :directory
     end
 
     test "if given path is valid and contains many levels and some levels are already present but they are not directories, returns {:error, {:invalid_type, path, type}}",
---
+++ test/support/ssh_daemon.exs
@@ -0,0 +1,71 @@
+defmodule SFTPToolkit.Support.SSHDaemon do
+  def make_tempdir do
+    tempdir =
+      cond do
+        !is_nil(System.get_env("TMPDIR")) ->
+          System.get_env("TMPDIR")
+
+        !is_nil(System.get_env("TMP")) ->
+          System.get_env("TMP")
+
+        !is_nil(System.get_env("TEMP")) ->
+          System.get_env("TEMP")
+
+        true ->
+          "/tmp"
+      end
+      |> Path.join("sftp_toolkit_test")
+      |> Path.join(to_string(Enum.random(1_000_000..9_999_999)))
+
+    if File.exists?(tempdir) do
+      make_tempdir()
+    else
+      {:ok, tempdir}
+    end
+  end
+
+
+  defmacro __using__([]) do
+    quote do
+      setup do
+        {:ok, tempdir} = SFTPToolkit.Support.SSHDaemon.make_tempdir()
+
+        :ok = File.mkdir_p!(tempdir)
+
+        {:ok, daemon_ref} =
+          {:ok, ssh_daemon_ref} =
+          :ssh.daemon(:loopback, 0,
+            user_passwords: [{'someuser', 'somepassword'}],
+            system_dir: Path.join([System.cwd!(), "test", "extra", "ssh"]) |> to_charlist,
+            subsystems: [
+              :ssh_sftpd.subsystem_spec(cwd: tempdir |> to_charlist)
+            ]
+          )
+
+        {:ok, [port: port, ip: ip, profile: :default]} = :ssh.daemon_info(daemon_ref)
+
+        {:ok, sftp_channel_pid, ssh_connection_ref} =
+          :ssh_sftp.start_channel(ip, port,
+            user: 'someuser',
+            password: 'somepassword',
+            user_interaction: false,
+            silently_accept_hosts: true
+          )
+
+        on_exit(:ssh_client, fn ->
+          :ssh.close(ssh_connection_ref)
+        end)
+
+        on_exit(:ssh_daemon, fn ->
+          :ssh.stop_daemon(ssh_daemon_ref)
+        end)
+
+        on_exit(:del, fn ->
+          File.rm_rf!(tempdir)
+        end)
+
+        [sftp_channel_pid: sftp_channel_pid, tempdir: tempdir]
+      end
+    end
+  end
+end
